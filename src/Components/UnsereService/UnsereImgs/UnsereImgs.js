148d8fcd1698af4a41c04e5459783266efa4a376
ItsMeDarai <95436006+ItsMeDarai@users.noreply.github.com>
2023-02-14 12:57:40 +0100

    Finished main part of Unsere Service
    added photos to unsere service

--- src/Components/UnsereService/UnsereImgs/UnsereImgs.js
+++ src/Components/UnsereService/UnsereImgs/UnsereImgs.js
@@ -1,11 +1,20 @@
 import React from "react";
 import styles from './UnsereImgs.module.scss';
+import right from '../../../Images/UnsereService/right.png';
+import middle from '../../../Images/UnsereService/middle.png';
+import left from '../../../Images/UnsereService/left.png';
 
 const UnsereImgs = () => (
     <div className={styles.wrapper}>
-        <div className={styles.img3} />
-        <div className={styles.img2} />
-        <div className={styles.img1} />
+        <div className={styles.img3}> 
+             <img src={right} />
+        </div>
+        <div className={styles.img2}> 
+             <img src={left} />
+        </div>
+        <div className={styles.img1}> 
+             <img src={middle} />
+        </div>
     </div>
 );
 
--- src/Components/UnsereService/UnsereService.js
+++ src/Components/UnsereService/UnsereService.js
@@ -3,9 +3,13 @@ import MehrAbs from "./MehrAbs/MehrAbs";
 import UnsereImgs from "./UnsereImgs/UnsereImgs";
 import UnsereMain from "./UnsereMain/UnsereMain";
 import styles from './UnsereService.module.scss';
+import vectorLeft from '../../Images/UnsereService/vector_left.png';
+import vectorRight from '../../Images/UnsereService/vector_right.png';
 
 const UnsereService = () => (
     <div className={styles.wrapper}>
+        <img className={styles.vectorLeft} src={vectorLeft} />
+        <img className={styles.vectorRight} src={vectorRight} />
         <div className={styles.innerWrapper}>
             <UnsereMain />
             <UnsereImgs />
