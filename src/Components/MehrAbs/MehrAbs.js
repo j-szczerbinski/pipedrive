c5346f772eaa5cafb8e33c943e209bd5a2ea453f
ItsMeDarai <95436006+ItsMeDarai@users.noreply.github.com>
2023-02-15 14:43:17 +0100

    updated MehrAbs
    updated MehrAbs according to BEM

--- src/Components/MehrAbs/MehrAbs.js
+++ src/Components/MehrAbs/MehrAbs.js
@@ -3,12 +3,12 @@ import styles from './MehrAbs.module.scss';
 import magnet from '../../Images/MehrAbs/magnet.png';
 
 const MehrAbs = () => (
-    <div className={styles.wrapper}>
-        <div className={styles.innerWrapper}>
-            <div className={styles.text}>
+    <div className={styles.MehrAbs}>
+        <div className={styles.MehrAbs__wrapper}>
+            <div className={styles.MehrAbs__wrapper__text}>
             Mehr Abschlüsse und <span>Effizienz mit unseren<br/> maßgeschneiderten</span> Pipedrive Lösungen
             </div>
-            <div className={styles.image}>
+            <div className={styles.MehrAbs__wrapper__image}>
                 <img src={magnet} />
             </div>
         </div>
