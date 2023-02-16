6981aac139d4af7ae643847924b78d24bcb8096b
ItsMeDarai <95436006+ItsMeDarai@users.noreply.github.com>
2023-02-16 10:28:24 +0100

    redone mehrAbs
    changed class names

--- src/Components/MehrAbs/MehrAbs.js
+++ src/Components/MehrAbs/MehrAbs.js
@@ -3,16 +3,16 @@ import styles from './MehrAbs.module.scss';
 import magnet from '../../Images/MehrAbs/magnet.png';
 
 const MehrAbs = () => (
-    <div className={styles.MehrAbs}>
-        <div className={styles.MehrAbs__wrapper}>
-            <div className={styles.MehrAbs__wrapper__text}>
-            Mehr Abschlüsse und <span>Effizienz mit unseren<br/> maßgeschneiderten</span> Pipedrive Lösungen
+    <section className={styles.mehrAbs}>
+        <div className={styles.mehrAbs__container}>
+            <div className={styles.mehrAbs__text}>
+                Mehr Abschlüsse und <span>Effizienz mit unseren<br/> maßgeschneiderten</span> Pipedrive Lösungen
             </div>
-            <div className={styles.MehrAbs__wrapper__image}>
+            <div className={styles.mehrAbs__image}>
                 <img src={magnet} />
             </div>
         </div>
-    </div>
+    </section>
 );
 
 export default MehrAbs;
