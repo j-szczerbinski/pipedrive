5c75b6f6a9ef97df9086b1969fcb36ac62771a3d
ItsMeDarai <95436006+ItsMeDarai@users.noreply.github.com>
2023-02-16 10:21:27 +0100

    redone unsereService
    redone the layout and scss file

--- src/Components/UnsereService/UnsereService.js
+++ src/Components/UnsereService/UnsereService.js
@@ -1,22 +1,32 @@
 import React from "react";
-import MehrAbs from "../MehrAbs/MehrAbs";
-import UnsereImgs from "./UnsereImgs/UnsereImgs";
-import UnsereMain from "./UnsereMain/UnsereMain";
+import mainImg from '../../Images/UnsereService/mainImg.png';
 import styles from './UnsereService.module.scss';
 import vectorLeft from '../../Images/UnsereService/vector_left.png';
 import vectorRight from '../../Images/UnsereService/vector_right.png';
+import Button from '../Common/Button/Button';
 
 const UnsereService = () => (
-    <div className={styles.UnsereService}>
-        <div className={styles.UnsereService__wrapper}>
-            <img className={styles.UnsereService__wrapper__vectorLeft} src={vectorLeft} />
-            <img className={styles.UnsereService__wrapper__vectorRight} src={vectorRight} />
-            <div className={styles.UnsereService__wrapper__main}>
-                <UnsereMain />
-                <UnsereImgs />
+    <section className={styles.unsereService}>
+        <div className={styles.unsereService__container}>
+            <h1 className={styles.unsereService__h1}>On-Boarding, Automation & persönliche Beratung</h1>
+            <h2 className={styles.unsereService__h2}>Mehr Abschlüsse und Effizienz mit unseren maßgeschneiderten Pipedrive Lösungen</h2>
+            <div className={styles.unsereService__buttonsContainer}>
+                <Button 
+                    text="Gespräch vereinbaren"
+                    class={styles.unsereService__leftButton}
+                    onClick={() => alert("pressed the left button")}
+                />
+                <Button
+                    text="30 Tage testen"
+                    class={styles.unsereService__rightButton}
+                    onClick={() => alert("pressed the right button")}
+                />
             </div>
+            <img className={styles.unsereService__img} src={mainImg} />
+            <img className={styles.unsereService__vectorLeft} src={vectorLeft} />
+            <img className={styles.unsereService__vectorRight} src={vectorRight} />
         </div>
-    </div>
+    </section>
 );
 
 export default UnsereService;
