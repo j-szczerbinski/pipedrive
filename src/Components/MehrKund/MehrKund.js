d388e28eb3cdb405b2ca3b59ec70caec47c15b63
ItsMeDarai <95436006+ItsMeDarai@users.noreply.github.com>
2023-02-16 10:57:22 +0100

    redone MehrKund
    chnaged class names and layout

--- src/Components/MehrKund/MehrKund.js
+++ src/Components/MehrKund/MehrKund.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from './MehrKund.module.scss';
 import MehrKundTile from "./MehrKundTile/MehrKundTile";
-import MehrKundUpper from "./MehrKundUpper/MehrKundUpper";
 import leftImage from "../../Images/MehrKund/left.png";
 import leftMiddleImage from "../../Images/MehrKund/left_middle.png";
 import rightMiddleImage from "../../Images/MehrKund/right_middle.png";
@@ -9,35 +8,38 @@ import rightImage from "../../Images/MehrKund/right.png";
 
 
 const MehrKund = (props) => (
-    <div className={styles.MehrKund}>
-        <MehrKundUpper />
-        <div className={styles.MehrKund__tiles}>
+    <section className={styles.mehrKund}>
+        <div className={styles.mehrKund__textContainer}>
+            <h1 className={styles.mehrKund__h1}>Mehr Kundenabschlüss</h1>
+            <h2 className={styles.mehrKund__h2}>Mehr Abschlüsse und Effizienz mit unseren maßgeschneiderten Pipedrive Lösungen.</h2>
+        </div>
+        <div className={styles.mehrKund__tilesContainer}>
             <MehrKundTile 
-            class={styles.MehrKund__tiles__tile}
-            image={leftImage}
-            headText="Mehr Kundenabschlüsse"
-            subText="Effizientere Bearbeitung, Kunden individuell Ansprechen, alle relevanten"
+                class={styles.mehrKund__tile}
+                image={leftImage}
+                headText="Mehr Kundenabschlüsse"
+                subText="Effizientere Bearbeitung, Kunden individuell Ansprechen, alle relevanten"
             />
             <MehrKundTile 
-            class={styles.MehrKund__tiles__tile}
-            image={leftMiddleImage}
-            headText="Vollständige Transparenz"
-            subText="Zeit, Leerlauf, Klare Struktur, alle relevanten Informationen zu Leads an einem Ort,"
+                class={styles.mehrKund__tile}
+                image={leftMiddleImage}
+                headText="Vollständige Transparenz"
+                subText="Zeit, Leerlauf, Klare Struktur, alle relevanten Informationen zu Leads an einem Ort,"
             />
             <MehrKundTile 
-            class={styles.MehrKund__tiles__tile}
-            image={rightMiddleImage}
-            headText="Motivierteres Vertriebsteam"
-            subText="Zielsetzung, einfacher Informationsaustausch über Kunden"
+                class={styles.mehrKund__tile}
+                image={rightMiddleImage}
+                headText="Motivierteres Vertriebsteam"
+                subText="Zielsetzung, einfacher Informationsaustausch über Kunden"
             />
             <MehrKundTile 
-            class={styles.MehrKund__tiles__tile}
-            image={rightImage}
-            headText="Volle Flexibilität"
-            subText="Hält mit deinem Wachstum Schritt, Individualisierte Lösungen"
+                class={styles.mehrKund__tile}
+                image={rightImage}
+                headText="Volle Flexibilität"
+                subText="Hält mit deinem Wachstum Schritt, Individualisierte Lösungen"
             />
         </div>
-    </div>
+    </section>
 );
 
 export default MehrKund;
