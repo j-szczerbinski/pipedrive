15dfef40231402af7b7aa7435a6004fdad6a20d1
ItsMeDarai <95436006+ItsMeDarai@users.noreply.github.com>
2023-02-17 16:55:10 +0100

    redone the header/navbar
    changed layout and removed the navlinks component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import NavLink from './NavLinks/NavLinks';
-import Logo from '../Logo/Logo';
 import Button from '../Common/Button/Button';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const Header = () => (
     <nav className={styles.header}>
-        <Logo />
-        <div className={styles.header__links}>
-            <NavLink />
-        </div>    
-        <Button 
-            text="Gespräch vereinbaren"
-            class={styles.header__button}
-            onClick={() => alert("pressed the header button")}
-        />
+        <div className={styles.header__container}>
+            <div className={styles.header__logo}>Pipedrive Masters</div>
+            <div className={styles.header__menu}>
+                <BrowserRouter>
+                    <Routes>
+                        {/* <Route path="/" element={<UnserService />} />
+                        <Route path="/" element={<Ablauf />} />
+                        <Route path="/" element={<CaseStudies />} />
+                        <Route path="/" element={<Pricing />} />
+                        <Route path="/" element={<FAQs />} /> */}
+                    </Routes>
+                    <Link className={styles.header__link} to="/UnserService">Unser Service</Link>
+                    <Link className={styles.header__link} to="/Ablauf">Ablauf</Link>
+                    <Link className={styles.header__link} to="/CaseStudies">Case Studies</Link>
+                    <Link className={styles.header__link} to="/Pricing">Pricing</Link>
+                    <Link className={styles.header__link} to="/FAQs">FAQs</Link>
+                </BrowserRouter>
+            </div>    
+            <Button 
+                text="Gespräch vereinbaren"
+                class={styles.header__button}
+                onClick={() => alert("pressed the header button")}
+            />
+        </div>
     </nav>
 );
 
